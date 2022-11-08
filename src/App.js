a23e2351652d35bb0d492721ccb3bba65985d32e
mengwp2004 <mengwp_2012@163.com>
2022-11-08 11:30:03 +0800

    nft-study-react-query
    Signed-off-by: mengwp2004 <mengwp_2012@163.com>

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useQuery, gql } from '@apollo/client';
+
+
+const GET_LOCATIONS = gql`
+query GetGravatars {
+  gravatars(first: 5) {
+    id
+    owner
+    displayName
+    imageUrl
+  }
+}
+`;
+
+function GetGravatars() {
+  const { loading, error, data } = useQuery(GET_LOCATIONS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.gravatars.map(({ id, owner, displayName, imageUrl }) => (
+    <div key={id}>
+      <b>owner:</b>
+      <h3>{owner}</h3>
+      <b>displayName:</b>
+      <p>{displayName}</p>
+      <b>imageUrl:</b>
+      <p>{imageUrl}</p>
+      <br />
+      <br />
+    </div>
+  ));
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>nft study gravators 🚀</h2>
+      <br/>
+      <GetGravatars />
     </div>
   );
 }
+
+
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,36 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+
+const client = new ApolloClient({
+  //uri: 'https://flyby-gateway.herokuapp.com/',
+  uri: 'https://api.thegraph.com/subgraphs/name/mengwp2004/nftstudy',
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query GetGravatars {
+        gravatars(first: 5) {
+          id
+          owner
+          displayName
+          imageUrl
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
+  </ApolloProvider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
